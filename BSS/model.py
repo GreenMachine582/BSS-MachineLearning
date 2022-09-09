542c24818ff58bf2c340e6eb63b8a40105da8f43
Matthew <greenchicken1902@gmail.com>
2022-09-09 12:30:09 +1000

    Fixed save function as it was using the wrong variables Minor changes

--- BSS/model.py
+++ BSS/model.py
@@ -9,28 +9,29 @@ from BSS import utils
 
 class Model(object):
 
-    def __init__(self, config: Config, **kwargs: dict):
+    def __init__(self, config: Config, **kwargs: Any | dict):
         self.config = config
 
         self.model = None
 
-        self._dir = self.config.model_dir
+        self.dir_ = self.config.working_dir + '\\models'
         self.name = self.config.name
-        self.extension = self.config.model_extension
+        self.extension = '.model'
 
         self.update(**kwargs)
 
-    def update(self, **kwargs: dict) -> None:
+    def update(self, **kwargs: Any | dict) -> None:
         """
         Updates the class attributes with given keys and values.
-        :param kwargs: dict[str: Any]
+        :param kwargs: Any | dict[str: Any]
         :return:
             - None
         """
-        logging.info("Updating attributes")
         if 'kwargs' in kwargs and isinstance(kwargs['kwargs'], dict):
             kwargs = kwargs['kwargs']
 
+        name = self.name if 'name' not in kwargs else kwargs['name']
+        logging.info(f"Updating '{name}' model attributes")
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -40,7 +41,7 @@ class Model(object):
         :return:
             - completed - bool
         """
-        path, exist = utils.checkPath(f"{self._dir}\\{self.name}", self.extension)
+        path, exist = utils.checkPath(f"{self.dir_}\\{self.name}", self.extension)
         if exist:
             logging.info(f"Loading model '{path}'")
             self.model = pickle.load(open(path, "rb"))
@@ -55,9 +56,9 @@ class Model(object):
         :return:
             - None
         """
-        if not utils.checkPath(self.config.model_dir):
-            os.makedirs(self.config.model_dir)
-        path, _ = utils.checkPath(f"{self._dir}\\{self.name}", self.extension)
+        if not utils.checkPath(self.dir_):
+            os.makedirs(self.dir_)
+        path, _ = utils.checkPath(f"{self.dir_}\\{self.name}", self.extension)
 
         logging.info(f"Saving file '{path}'")
         pickle.dump(self.model, open(path, "wb"))
