0814d5a84c94fe70364cf294e33c7953cf9c39dd
Matthew <greenchicken1902@gmail.com>
2022-09-12 13:37:40 +1000

    Changed the model from RandomForestRegressor to Gradient BoostingRegressor in trainModel Reworked the extractFeatures method by moving lines from processData Minor changes

--- examples/test.py
+++ examples/test.py
@@ -1,24 +1,22 @@
 
 import logging
-import math
 import os
-
-import BSS
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sn
 
-from sklearn.model_selection import train_test_split, TimeSeriesSplit, cross_val_score, KFold, GridSearchCV
+from sklearn.model_selection import TimeSeriesSplit, cross_val_score, GridSearchCV
 from sklearn import metrics
-from sklearn.neighbors import KNeighborsRegressor, KNeighborsClassifier
-from sklearn.linear_model import LinearRegression
-from sklearn.neural_network import MLPRegressor, MLPClassifier
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.svm import SVR
 from sklearn.cluster import SpectralBiclustering, AgglomerativeClustering
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.neural_network import MLPRegressor
+from sklearn.svm import SVR
+
+import BSS
 
 # Constants
 local_dir = os.path.dirname(__file__)
@@ -26,54 +24,59 @@ local_dir = os.path.dirname(__file__)
 
 def processData(config, dataset):
     logging.info("Processing data")
-    print(dataset.head())
+    df = dataset.df
 
-    # for i in range(dataset.shape[0]):
-    #     label = dataset[config.target].iloc[i]
-    #     dataset[config.target].iloc[i] = math.ceil(label / 10) * 10  # decrease the number of categories
+    print(df.head())
 
-    # dataset[config.target] = dataset[config.target].astype('category')
+    df['dteday'] = pd.to_datetime(df['dteday'])
+    df.index = df['dteday']
 
-    dataset['dteday'] = pd.to_datetime(dataset['dteday'])
-    dataset.index = dataset['dteday']
+    df = df.drop(['instant', 'dteday', 'casual', 'registered'], axis=1)
 
-    dataset = dataset.drop(['instant', 'dteday', 'registered', 'casual'], axis=1)  # temp remove
+    dataset.update(df=df, suffix='-processed')
+    dataset.handleMissingData()
 
-    # dataset = dataset[['cnt']]
-    # dataset.loc[:, 'prev'] = dataset.loc[:, 'cnt'].shift()
-    # dataset.loc[:, 'diff'] = dataset.loc[:, 'prev'].diff()
+    df = dataset.df
 
-    dataset = dataset.dropna()
+    print(df.axes)
+    print(df.head())
+    print(df.dtypes)
 
-    X = dataset.drop(config.target, axis=1)  # denotes independent features
-    y = dataset[config.target]  # denotes dependent variables
+    x = df.drop(config.target, axis=1)  # denotes independent features
+    print("X shape:", x.shape)
 
-    print(dataset.axes)
-    print(dataset.head())
-
-    print(dataset.isnull().sum())  # check for missing values
-    print(dataset.dtypes)
-
-    print("X shape:", X.shape)
-
-    return dataset, X, y
+    return dataset
 
 
-def exploratoryDataAnalysis(dataset, x, y):
+def exploratoryDataAnalysis(dataset):
     logging.info("Exploratory Data Analysis")
+    df = dataset.df
+
     # plots a corresponding matrix
     plt.figure()
-    sn.heatmap(dataset.corr(), annot=True)
-
-    # plots a bar graph to represent number of instances per target/label
-    # plt.figure()
-    # y.value_counts().plot(kind="bar")
-
-    # plt.show()
+    sn.heatmap(df.corr(), annot=True)
 
 
-def extractFeatures(dataset, x, y):
+def extractFeatures(config, dataset):
     logging.info("Extracting features")
+    df = dataset.df
+
+    # adds some historical data
+    df.loc[:, 'prev'] = df.loc[:, 'cnt'].shift()
+    df.loc[:, 'diff'] = df.loc[:, 'prev'].diff()
+    df.loc[:, 'prev-2'] = df.loc[:, 'prev'].shift()
+    df.loc[:, 'diff-2'] = df.loc[:, 'prev-2'].diff()
+
+    df = df.drop(['mnth', 'season'], axis=1)
+
+    dataset.update(df=df, suffix='-extracted')
+    dataset.handleMissingData()
+
+    x = dataset.df.drop(config.target, axis=1)  # denotes independent features
+    y = dataset.df[config.target]  # denotes dependent variables
+
+    print(x.head())
+    print(y.head())
 
     return dataset, x, y
 
@@ -81,35 +84,30 @@ def extractFeatures(dataset, x, y):
 def splitDataset(config, x, y):
     logging.info("Splitting data")
 
-    # X_train, X_test, y_train, y_test = train_test_split(x, y, train_size=config.split_ratio,
-    #                                                     random_state=config.random_seed)
-
     size = round(x.shape[0] * config.split_ratio)
-    X_train = x[:size]
+    x_train = x[:size]
     y_train = y[:size]
-    X_test = x[size:]
+    x_test = x[size:]
     y_test = y[size:]
 
-    return X_train, X_test, y_train, y_test
+    return x_train, x_test, y_train, y_test
 
 
 def compareModels(x_train, y_train):
+    # TODO: separate the clustering techniques and apply appropriate prediction
+    #  and scoring method
     logging.info("Training model")
     models, names, results = [], [], []
     models.append(('LR', LinearRegression()))
     models.append(('NN', MLPRegressor(solver='lbfgs')))  # neural network
     models.append(('KNN', KNeighborsRegressor()))
     models.append(('RF', RandomForestRegressor(n_estimators=10)))  # Ensemble method - collection of many decision trees
-    models.append(('SVR', SVR(gamma='auto')))  # kernel = linear
-    #models.append(('BIC', SpectralBiclustering(n_clusters=(4, 3))))
-    models.append(('AC', AgglomerativeClustering(n_clusters=4)))
-    models.append(('TRE', DecisionTreeClassifier(criterion='entropy', min_samples_split=25)))
-    models.append(('NNC', MLPClassifier(solver='lbfgs')))
-
+    # models.append(('SVR', SVR(gamma='auto')))  # kernel = linear
+    # models.append(('BIC', SpectralBiclustering(n_clusters=(4, 3))))
+    # models.append(('AC', AgglomerativeClustering(n_clusters=4)))
 
     for name, model in models:
         tscv = TimeSeriesSplit(n_splits=10)  # TimeSeries Cross validation
-        kfold = KFold(n_splits=5)
 
         cv_results = cross_val_score(model, x_train, y_train, cv=tscv, scoring='r2')
         results.append(cv_results)
@@ -123,22 +121,6 @@ def compareModels(x_train, y_train):
     plt.show()
 
 
-def trainModel(x_train, y_train):
-    rmse_score = metrics.make_scorer(rmse, greater_is_better=False)
-    model = RandomForestRegressor()
-    param_search = {
-        'n_estimators': [20, 50, 100],
-        'max_features': ['auto', 'sqrt', 'log2'],
-        'max_depth': [i for i in range(5, 15)]
-    }
-    tscv = TimeSeriesSplit(n_splits=10)
-    gsearch = GridSearchCV(estimator=model, cv=tscv, param_grid=param_search, scoring=rmse_score)
-    gsearch.fit(x_train, y_train)
-    best_model = gsearch.best_estimator_
-    best_score = gsearch.best_score_
-    return best_model, best_score
-
-
 def rmse(actual, predict):
     predict = np.array(predict)
     actual = np.array(actual)
@@ -147,6 +129,20 @@ def rmse(actual, predict):
     mean_square_distance = square_distance.mean()
     score = np.sqrt(mean_square_distance)
     return score
+
+
+def trainModel(x_train, y_train):
+    model = GradientBoostingRegressor()
+    param_search = {'learning_rate': [0.01, 0.02, 0.03, 0.04],
+                    'subsample': [0.9, 0.5, 0.2, 0.1],
+                    'n_estimators': [100, 500, 1000, 1500],
+                    'max_depth': [4, 6, 8, 10]}
+    tscv = TimeSeriesSplit(n_splits=10)
+    gsearch = GridSearchCV(estimator=model, cv=tscv, param_grid=param_search, n_jobs=-1)
+    gsearch.fit(x_train, y_train)
+    best_model = gsearch.best_estimator_
+    best_score = gsearch.best_score_
+    return best_model, best_score
 
 
 def regression_results(y_test, y_pred):
@@ -165,7 +161,7 @@ def regression_results(y_test, y_pred):
     print('RMSE: ', round(np.sqrt(mse), 4))
 
 
-def resultAnalysis(model, score, x_train, y_train, x_test, y_test):
+def resultAnalysis(model, score, x_test, y_test):
     logging.info("Analysing results")
 
     print("Score - %.4f%s" % (score * 100, "%"))
@@ -175,34 +171,34 @@ def resultAnalysis(model, score, x_train, y_train, x_test, y_test):
     regression_results(y_test, y_pred)
 
 
-def main(dir_=''):
+def main(dir_=local_dir):
     config = BSS.Config(dir_, dataset_name='Bike-Sharing-Dataset-day', model_technique='test',
                         model_algorithm='all')
 
-    raw_dataset = BSS.Dataset(config)
-    raw_dataset.load()
+    dataset = BSS.Dataset(config)
+    dataset.load()
 
-    if raw_dataset.dataset is None:
+    if dataset.df is None:
         logging.error("Couldn't load a dataset")
 
-    dataset, X, y = processData(config, raw_dataset.dataset)
+    dataset = processData(config, dataset)
+    exploratoryDataAnalysis(dataset)
 
-    exploratoryDataAnalysis(dataset, X, y)
+    dataset, x, y = extractFeatures(config, dataset)
+    exploratoryDataAnalysis(dataset)
 
-    dataset, X, y = extractFeatures(dataset, X, y)
+    x_train, x_test, y_train, y_test = splitDataset(config, x, y)
 
-    X_train, X_test, y_train, y_test = splitDataset(config, X, y)
+    compareModels(x_train, y_train)
 
-    compareModels(X_train, y_train)
-
-    model, score = trainModel(X_train, y_train)
+    model, score = trainModel(x_train, y_train)
 
     BSS.Model(config, model=model).save()
 
-    resultAnalysis(model, score, X_train, y_train, X_test, y_test)
+    resultAnalysis(model, score, x_test, y_test)
 
     return
 
 
 if __name__ == '__main__':
-    main(local_dir)
+    main()
