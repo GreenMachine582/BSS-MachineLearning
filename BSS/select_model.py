52c6b81e619d9d6f742209817e28a23593039f55
Matthew <greenchicken1902@gmail.com>
2022-09-30 18:07:39 +1000

    Reworked convertToCategorical as the classes were previously imbalanced Added more estimator and classifier models

--- BSS/select_model.py
+++ BSS/select_model.py
@@ -1,19 +1,12 @@
 from __future__ import annotations
 
 import logging
-import math
 import os
 
 from matplotlib import pyplot as plt
 from pandas import DataFrame
-from sklearn.cluster import AgglomerativeClustering, SpectralBiclustering
-from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor
-from sklearn.linear_model import LinearRegression, LogisticRegression
-from sklearn.metrics import accuracy_score
+from sklearn import ensemble, linear_model, neighbors, neural_network, svm, tree
 from sklearn.model_selection import TimeSeriesSplit, cross_val_score
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.neural_network import MLPRegressor
-from sklearn.svm import SVR
 
 import BSS
 import machine_learning as ml
@@ -22,62 +15,67 @@ import machine_learning as ml
 local_dir = os.path.dirname(__file__)
 
 
-def convertToCategorical(df: DataFrame, future_categories: int = 1, num_of_categories: int = 20) -> DataFrame:
-    # TODO: Documentation
-    if future_categories > num_of_categories - 1:
-        logging.warning(f"Number_of_categories must be larger than future_categories")
-        return df
+def convertToCategorical(df: DataFrame) -> DataFrame:
+    """
+    Converts the BSS demand (cnt) into a binary category where,
+    0 represents a fall in demand from previous instance
+    1 represents a rise in demand from previous instance.
+    :param df: The BSS dataset, should be a DataFrame
+    :return: df - DataFrame
+    """
+    df['cnt'] = [0 if df['diff'][i] < 0 else 1 for i in range(len(df['cnt']))]
 
-    interval = math.floor(max(df['cnt']) / ((num_of_categories - 1) - future_categories))
+    # Checks for class imbalance
+    if len(df['cnt'] == 0) != len(df['cnt'] == 1):
+        logging.warning("Class imbalance is present")
 
-    def temp(x):
-        for i in range(num_of_categories):
-            if x < i * interval:
-                return i * interval
-
-    df['cnt'] = df['cnt'].apply(temp)
     df['cnt'] = df['cnt'].astype("category")
     return df
 
 
-def getContinuousModels(random_seed: int = None) -> list:
-    models = [('LR', LinearRegression()),
-              ('NN', MLPRegressor(max_iter=1200, random_state=random_seed)),
-              ('KNN', KNeighborsRegressor()),
-              ('RF', RandomForestRegressor(random_state=random_seed)),
-              ('SVR', SVR()),
-              ('GBR', GradientBoostingRegressor(random_state=random_seed))]
+def getEstimators(random_state: int = None) -> list:
+    models = [('EGBR', ensemble.GradientBoostingRegressor(random_state=random_state)),
+              ('ERFR', ensemble.RandomForestRegressor(random_state=random_state)),
+              ('NKNR', neighbors.KNeighborsRegressor()),
+              ('NNMLPR', neural_network.MLPRegressor(max_iter=1200, random_state=random_state)),
+              ('SSVR', svm.SVR()),
+              ('TDTR', tree.DecisionTreeRegressor(random_state=random_state)),
+              ('TETR', tree.ExtraTreeRegressor(random_state=random_state))]
     return models
 
 
-def getCategoricalModels(random_seed: int = None) -> list:
-    models = [('LR', LogisticRegression()),]
-              # ('AC', AgglomerativeClustering()),
-              # ('BIC', SpectralBiclustering(random_state=random_seed))]
+def getClassifiers(random_state: int = None) -> list:
+    models = [('EGBC', ensemble.GradientBoostingClassifier(random_state=random_state)),
+              ('ERFC', ensemble.RandomForestClassifier(random_state=random_state)),
+              ('LMLR', linear_model.LogisticRegression(random_state=random_state)),
+              ('LMRC', linear_model.RidgeClassifier(random_state=random_state)),
+              ('LMSGDC', linear_model.SGDClassifier(random_state=random_state)),
+              ('NKNC', neighbors.KNeighborsClassifier()),
+              ('NNC', neighbors.NearestCentroid()),
+              ('NNMLPC', neural_network.MLPClassifier(max_iter=1200, random_state=random_state)),
+              ('SSVC', svm.SVC()),
+              ('TDTC', tree.DecisionTreeClassifier(random_state=random_state))]
     return models
 
 
-def compareModels(X, y, models, scoring: str = None, cv: int | object = 10):
+def compareModels(X: DataFrame, y: DataFrame, models: list, cv: int | object = 10) -> dict:
     """
     Trains and cross validates a basic model with default params.
 
-    :param X: independent features, should be a DataFrame
-    :param y: dependent variables, should be a DataFrame
+    :param X: Independent features, should be a DataFrame
+    :param y: Dependent variables, should be a DataFrame
     :param models:
-    :param scoring:
     :param cv:
-    :return: cv_results - ndarray[float]
+    :return: results - dict[str: cv_results]
     """
     # TODO: Documentation
     logging.info("Comparing models")
-    names, results = [], []
-
+    results = {}
     for name, model in models:
-        cv_results = cross_val_score(model, X, y, scoring=scoring, cv=cv)
-        results.append(cv_results)
-        names.append(name)
+        cv_results = cross_val_score(model, X, y, cv=cv)
+        results[name] = cv_results
         print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
-    return results, names
+    return results
 
 
 def main(dir_: str = local_dir) -> None:
@@ -96,14 +94,14 @@ def main(dir_: str = local_dir) -> None:
 
     dataset = BSS.processDataset(dataset)
 
-    logging.info('Selecting Dependent Feature Type')
+    logging.info('Select Group of Models')
     while True:
         print("""
             0 - Back
-            1 - Continuous
-            2 - Categorical
+            1 - Estimators
+            2 - Classifiers
             """)
-        choice = input("Which question number: ")
+        choice = input("Which option number: ")
         try:
             choice = int(choice)
         except ValueError:
@@ -114,36 +112,44 @@ def main(dir_: str = local_dir) -> None:
             if choice == 0:
                 return
             elif choice == 1:
-                models = getContinuousModels(config.random_seed)
+                models = getEstimators(config.random_state)
                 break
             elif choice == 2:
                 dataset.apply(convertToCategorical)
-                models = getCategoricalModels(config.random_seed)
+                models = getClassifiers(config.random_state)
                 break
             else:
                 print("\nPlease enter a valid choice!")
 
-    X, y = dataset.getIndependent(), dataset.getDependent()
-    results, names = compareModels(X, y, models, cv=TimeSeriesSplit(n_splits=10))
+    X_train, X_test, y_train, y_test = dataset.split(random_state=config.random_state, shuffle=False)
+
+    results = compareModels(X_train, y_train, models, cv=TimeSeriesSplit(n_splits=10))
 
     plt.figure()
-    plt.boxplot(results, labels=names)
+    plt.boxplot(results.values(), labels=results.keys())
     plt.title('Algorithm Comparison')
     plt.show()
 
     # Remove models that performed poorly
     if choice == 1:
-        del results[4]
-        del names[4]
-        del results[0]
-        del names[0]
+        del results['SSVR']
+        del results['TDTR']
+        del results['TETR']
 
         plt.figure()
-        plt.boxplot(results, labels=names)
+        plt.boxplot(results.values(), labels=results.keys())
         plt.title('Algorithm Comparison')
         plt.show()
     else:
-        pass
+        del results['LMRC']
+        del results['NKNC']
+        del results['NNC']
+        del results['SSVC']
+
+        plt.figure()
+        plt.boxplot(results.values(), labels=results.keys())
+        plt.title('Algorithm Comparison')
+        plt.show()
 
     logging.info(f"Completed")
     return
